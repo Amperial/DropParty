dd24993f344b8fe51969779331c38b732e7604d1
Austin Payne <payneaustin5@gmail.com>
2013-04-27 21:25:57 -0500

    First Commit!

---
+++ src/main/java/me/ampayne2/DropParty/DropParty.java
@@ -0,0 +1,86 @@
+package me.ampayne2.DropParty;
+
+import java.util.ArrayList;
+
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.PluginManager;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class DropParty extends JavaPlugin {
+	
+    private final ArrayList<String> playersSelecting = new ArrayList<String>();
+	
+	public void onEnable(){
+		PluginManager manager = this.getServer().getPluginManager();
+		manager.registerEvents(new DropPartyListener(), this);
+	}
+	
+	public void onDisable(){
+		
+	}
+	
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+    	
+        if (!cmd.getName().equalsIgnoreCase("dropparty"))
+            return false;
+
+        if (!(sender instanceof Player)) {
+            sender.sendMessage("This command can only be run by a player.");
+            return false;
+        }
+
+        if (!sender.hasPermission("dropparty.admin"))
+            return false;
+
+        if (args.length == 0) {
+        	sender.sendMessage(ChatColor.RED + "Not enough arguments.");
+            return false;
+        }
+
+        if (args.length > 1) {
+            sender.sendMessage(ChatColor.RED + "Too many arguments.");
+            return false;
+        }
+
+        if (!args[0].equalsIgnoreCase("setchest") && !args[0].equalsIgnoreCase("setitem") && !args[0].equalsIgnoreCase("start") && !args[0].equalsIgnoreCase("stop")) {
+            sender.sendMessage(ChatColor.RED + "Invalid argument. Valid arguments are: setchest, setitem, start, and stop.");
+            return false;
+        }
+
+        String playerName = ((Player) sender).getDisplayName();
+
+        if (args[0].equals("setchest")) {
+            toggleSelecting(playerName, sender);
+            return true;
+        }
+        if (args[0].equals("setitem")) {
+            return true;
+        }
+        if (args[0].equals("start")) {
+            return true;
+        }
+        if (args[0].equals("stop")) {
+            return true;
+        }
+        return false;
+    }
+    public void toggleSelecting(String playerName, CommandSender sender)
+    {
+        if (isSelecting(playerName)) {
+            playersSelecting.remove(playerName);
+            sender.sendMessage("Selection mode disabled.");
+        }
+        else {
+            playersSelecting.add(playerName);
+            sender.sendMessage("Selection mode activated.");
+        }
+    }
+
+    public boolean isSelecting(String playerName) {
+        return playersSelecting.contains(playerName);
+    }
+}
---
+++ src/main/java/me/ampayne2/DropParty/DropPartyListener.java
@@ -0,0 +1,38 @@
+package me.ampayne2.DropParty;
+
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerInteractEvent;
+
+public class DropPartyListener implements Listener {
+	
+	public static DropParty plugin;
+
+    @EventHandler
+    public void onChestHit(PlayerInteractEvent event) {
+        if (event.getAction() != org.bukkit.event.block.Action.RIGHT_CLICK_BLOCK)
+            return;
+
+        if (!plugin.isSelecting(event.getPlayer().getDisplayName()))
+            return;
+
+        Block clickedBlock = event.getClickedBlock();
+
+        try {
+            if (clickedBlock.getType() != Material.CHEST)
+                return;
+        }
+        catch (NullPointerException ex) {
+            return;
+        }
+
+        Player player = event.getPlayer();
+        player.sendMessage(ChatColor.AQUA + "Chest Added");
+        event.setCancelled(true);
+    }
+	
+}
