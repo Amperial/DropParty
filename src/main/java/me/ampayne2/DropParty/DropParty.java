70b4bb5c5416f48e02c9aecc466602ed0fac8d1f
William Lebel <lebelwilliam@gmail.com>
2013-04-28 00:36:24 -0400

    Added SimpleSave support.

--- src/main/java/me/ampayne2/DropParty/DropParty.java
+++ src/main/java/me/ampayne2/DropParty/DropParty.java
@@ -2,6 +2,11 @@ package me.ampayne2.DropParty;
 
 import java.util.ArrayList;
 
+import com.alta189.simplesave.exceptions.ConnectionException;
+import com.alta189.simplesave.exceptions.TableRegistrationException;
+
+import me.ampayne2.DropParty.database.DatabaseManager;
+
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
@@ -12,11 +17,34 @@ import org.bukkit.plugin.java.JavaPlugin;
 public class DropParty extends JavaPlugin {
 
 	private final ArrayList<String> playersSelecting = new ArrayList<String>();
+	private DatabaseManager dbManager = null;
+	private static DropParty instance;
+
+
+	public DropParty getInstance() {
+		return instance;
+	}
 
 	public void onEnable() {
+		instance = this;
 		PluginManager manager = this.getServer().getPluginManager();
 		manager.registerEvents(new DropPartyListener(), this);
 		DropPartyListener.plugin = this;
+		getConfig().options().copyDefaults(true);
+		saveConfig();
+		try {
+			dbManager = new DatabaseManager(this);
+		} catch (TableRegistrationException e) {
+			getLogger().severe("A error occured while connecting to the database!");
+			e.printStackTrace();
+			getServer().getPluginManager().disablePlugin(this);
+			return;
+		} catch (ConnectionException e) {
+			getLogger().severe("A error occured while connecting to the database!");
+			e.printStackTrace();
+			getServer().getPluginManager().disablePlugin(this);
+			return;
+		}
 	}
 
 	public void onDisable() {
