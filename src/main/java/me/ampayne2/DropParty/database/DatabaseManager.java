00aa2782d7947ab421bf0d9c25cc255e607e8188
Austin Payne <payneaustin5@gmail.com>
2013-04-29 20:56:38 -0500

    Added GNU v3 license headers

--- src/main/java/me/ampayne2/DropParty/database/DatabaseManager.java
+++ src/main/java/me/ampayne2/DropParty/database/DatabaseManager.java
@@ -1,3 +1,21 @@
+/*
+ * This file is part of DropParty.
+ *
+ * Copyright (c) 2013-2013
+ *
+ * DropParty is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * DropParty is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with DropParty.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package me.ampayne2.DropParty.database;
 
 import com.alta189.simplesave.Database;
