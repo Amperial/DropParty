d77da0514a9b281b48e4c1ce3e511f87a189bbe2
Srđan Cokić <srdjanneon@gmail.com>
2019-08-11 22:18:48 +0200

    Create ConfigType.java

---
+++ src/main/java/us/divinerealms/neon/dropparty/config/ConfigType.java
@@ -0,0 +1,42 @@
+/*
+ * This file is part of DropParty.
+ *
+ * Copyright (c) 2013-2014 <http://dev.bukkit.org/server-mods/dropparty//>
+ *
+ * DropParty is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * DropParty is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with DropParty.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package us.divinerealms.neon.dropparty.config;
+
+/**
+ * An enumeration of the drop party custom config types.
+ */
+public enum ConfigType implements us.divinerealms.neon.amplib.config.ConfigType {
+    PARTY("parties.yml");
+
+    private final String fileName;
+
+    ConfigType(String fileName) {
+        this.fileName = fileName;
+    }
+
+    /**
+     * Gets the ConfigType's file name.
+     *
+     * @return The file name of the ConfigType.
+     */
+    public String getFileName() {
+        return fileName;
+    }
+
+}
